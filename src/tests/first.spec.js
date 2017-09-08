648dda698acee952f9092668272715a615b40674
LucasDeway <lucas.silva@deway.com.br>
2017-09-08 14:51:18 -0300

    Simulate input form with jest and enzyme

--- src/tests/first.spec.js
+++ src/tests/first.spec.js
@@ -22,6 +22,7 @@ describe('Testing <App /> Component!', () => {
       wrapper.contains(<th>Items</th>)
     ).toBe(true);
   });
+  // containsMatchingElement is for don't give in element all atributes how type, disabled and etc...
   it('should have a `button`', () => {
     expect(
       wrapper.containsMatchingElement(<button>Add item</button>)
@@ -34,10 +35,89 @@ describe('Testing <App /> Component!', () => {
       )
     ).toBe(true);
   });
+  // In button.props has all properties of button
   it('`button` should be disabled', () => {
     const button = wrapper.find('button').first();
     expect(
       button.props().disabled
     ).toBe(true);
   });
+
+  describe('The user action inpunt', () => {
+    const item = 'Eusébio';
+
+    beforeEach(() => {
+      const input = wrapper.find('input').first();
+      input.simulate('change', {
+        target: { value: item }
+      })
+    });
+
+    it('should update the state property `item`', () => {
+      expect(
+        wrapper.state().item
+      ).toEqual(item);
+    });
+
+    it('`Button` should be enabled', () => {
+      const button = wrapper.find('button').first();
+      expect(
+        button.props().disabled
+      ).toBe(false)
+    });
+
+    describe('and then clears the input', () => {
+      beforeEach(() => {
+        const input = wrapper.find('input').first();
+        input.simulate('change', {
+          target: { value: '' }
+        })
+      });
+
+      it('`Button` should be disabled', () => {
+        const button = wrapper.find('button').first();
+        expect(
+          button.props().disabled
+        ).toBe(true)
+      })
+    });
+
+    describe('and then submits the form', () => {
+      beforeEach(() => {
+        const form = wrapper.find('form').first();
+        form.simulate('submit', {
+          preventDefault: () => { },
+        });
+      });
+
+      it('should add the item to state', () => {
+        expect(
+          wrapper.state().items
+        ).toContain(item);
+      });
+
+      it('should render the item in the table', () => {
+        expect(
+          wrapper.containsMatchingElement(
+            <td>{item}</td>
+          )
+        ).toBe(true);
+      });
+
+      it('should clear the input field', () => {
+        const input = wrapper.find('input').first();
+        expect(
+          input.props().value
+        ).toEqual('');
+      });
+
+      it('`button` should be disabled', () => {
+        const button = wrapper.find('button').first();
+        expect(
+          button.props().disabled
+        ).toBe(true);
+      })
+    });
+  });
+
 });
